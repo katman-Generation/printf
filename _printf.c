21f250811673df7ecd665abc1f42c5f2f26166e2
NeoBoledi <75299402+NeoBoledi@users.noreply.github.com>
2022-04-22 00:38:05 +0200

    print f

--- _printf.c
+++ _printf.c
@@ -1,48 +1,82 @@
-#include "main.h"
+#include "holberton.h"
+#include <stdlib.h>
 
 /**
- * _printf - produces output according to a format
- * @format: format string containing the characters and the specifiers
- * Description: this function will call the get_print() function that will
- * determine which printing function to call depending on the conversion
- * specifiers contained into fmt
- * Return: length of the formatted output string
+ * check_for_specifiers - checks if there is a valid format specifier
+ * @format: possible format specifier
+ *
+ * Return: pointer to valid function or NULL
  */
+static int (*check_for_specifiers(const char *format))(va_list)
+{
+	unsigned int i;
+	print_t p[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{"i", print_i},
+		{"d", print_d},
+		{"u", print_u},
+		{"b", print_b},
+		{"o", print_o},
+		{"x", print_x},
+		{"X", print_X},
+		{"p", print_p},
+		{"S", print_S},
+		{"r", print_r},
+		{"R", print_R},
+		{NULL, NULL}
+	};
 
+	for (i = 0; p[i].t != NULL; i++)
+	{
+		if (*(p[i].t) == *format)
+		{
+			break;
+		}
+	}
+	return (p[i].f);
+}
+
+/**
+ * _printf - prints anything
+ * @format: list of argument types passed to the function
+ *
+ * Return: number of characters printed
+ */
 int _printf(const char *format, ...)
 {
-	int (*pfunc)(va_list, counter *);
-	const char *tate;
-	va_list neo;
-	counter cont = {0, 0, 0};
+	unsigned int i = 0, count = 0;
+	va_list valist;
+	int (*f)(va_list);
 
-	register int count = 0;
-
-	va_start(neo, format);
-	if (!format || (format[0] == '%' && !format[1]))
+	if (format == NULL)
 		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-	for (tate = format; *tate; tate++)
+	va_start(valist, format);
+	while (format[i])
 	{
-		if (*tate == '%')
+		for (; format[i] != '%' && format[i]; i++)
 		{
-			tate++;
-			if (*tate == '%')
-			{
-				count += _putchar('%');
-				continue;
-			}
-			while (get_flag(*tate, &cont))
-				tate++;
-			pfunc = get_print(*p);
-			count += (pfunc)
-				? pfunc(neo, &cont)
-				: _printf("%%%c", *tate);
+			_putchar(format[i]);
+			count++;
 		}
+		if (!format[i])
+			return (count);
+		f = check_for_specifiers(&format[i + 1]);
+		if (f != NULL)
+		{
+			count += f(valist);
+			i += 2;
+			continue;
+		}
+		if (!format[i + 1])
+			return (-1);
+		_putchar(format[i]);
+		count++;
+		if (format[i + 1] == '%')
+			i += 2;
 		else
-			count += _putchar(*tate);
+			i++;
 	}
-	_putchar(-1);
-	va_end(neo);
+	va_end(valist);
 	return (count);
+}
