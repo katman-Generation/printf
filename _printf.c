5c0bc3b634ea04904ef7775782daa217b6f25a9f
NeoBoledi <75299402+NeoBoledi@users.noreply.github.com>
2022-04-22 00:47:49 +0200

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -2,81 +2,51 @@
 #include <stdlib.h>
 
 /**
- * check_for_specifiers - checks if there is a valid format specifier
- * @format: possible format specifier
- *
- * Return: pointer to valid function or NULL
- */
-static int (*check_for_specifiers(const char *format))(va_list)
-{
-	unsigned int i;
-	print_t p[] = {
-		{"c", print_c},
-		{"s", print_s},
-		{"i", print_i},
-		{"d", print_d},
-		{"u", print_u},
-		{"b", print_b},
-		{"o", print_o},
-		{"x", print_x},
-		{"X", print_X},
-		{"p", print_p},
-		{"S", print_S},
-		{"r", print_r},
-		{"R", print_R},
-		{NULL, NULL}
-	};
-
-	for (i = 0; p[i].t != NULL; i++)
-	{
-		if (*(p[i].t) == *format)
-		{
-			break;
-		}
-	}
-	return (p[i].f);
-}
-
-/**
- * _printf - prints anything
- * @format: list of argument types passed to the function
- *
- * Return: number of characters printed
- */
+  *_printf - prints formatted output.
+  *@format: input.
+  *
+  *Return: number of chars printed or -1.
+  */
 int _printf(const char *format, ...)
 {
-	unsigned int i = 0, count = 0;
-	va_list valist;
-	int (*f)(va_list);
+	va_list args;
+	int i, len;
+	int (*get_ptr)(va_list, int);
 
-	if (format == NULL)
+	va_start(args, format);
+	if (!(format))
 		return (-1);
-	va_start(valist, format);
-	while (format[i])
+	i = 0;
+	len = 0;
+	while (format && format[i])
 	{
-		for (; format[i] != '%' && format[i]; i++)
+		if (format[i] == '%')
 		{
-			_putchar(format[i]);
-			count++;
-		}
-		if (!format[i])
-			return (count);
-		f = check_for_specifiers(&format[i + 1]);
-		if (f != NULL)
-		{
-			count += f(valist);
-			i += 2;
-			continue;
-		}
-		if (!format[i + 1])
-			return (-1);
-		_putchar(format[i]);
-		count++;
-		if (format[i + 1] == '%')
-			i += 2;
-		else
 			i++;
+			if (format[i] == '%')
+			{
+				len += _putchar(format[i]);
+				i++;
+				continue;
+			}
+			if (format[i] == '\0')
+				return (-1);
+			get_ptr = get_print_func(format[i]);
+			if (get_ptr != NULL)
+				len = get_ptr(args, len);
+			else
+			{
+				len += _putchar(format[i - 1]);
+				len += _putchar(format[i]);
+			}
+			i++;
+		}
+		else
+		{
+			len += _putchar(format[i]);
+			i++;
+		}
 	}
-	va_end(valist);
-	return (count);
+	va_end(args);
+	return (len);
 }
