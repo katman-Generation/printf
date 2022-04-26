7c49892ea85594c069bb41f1da3fd667bce48dfc
NeoBoledi <75299402+NeoBoledi@users.noreply.github.com>
2022-04-26 23:48:00 +0200

    Update _putchar.c

--- _putchar.c
+++ _putchar.c
@@ -1,11 +1,14 @@
 #include <unistd.h>
+
 /**
-  *_putchar - write to stdout.
-  *@c: char to be written.
-  *
-  *Return: number of chars written.
-  */
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+	write(1, &c, 1);
+	return (1);
 }
